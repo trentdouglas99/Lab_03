b0bbe1b09d66bd86779b8dbf9d955ae7d0ae72ca
trentdouglas99 <trentdouglas@mymail.mines.edu>
2021-02-04 19:50:04 -0700

    works now

--- app/src/main/java/com/csci448/trentdouglas/lab02/QuizApplication.kt
+++ app/src/main/java/com/csci448/trentdouglas/lab02/QuizApplication.kt
@@ -1,6 +1,9 @@
 package com.csci448.trentdouglas.lab02
 
 import android.app.Application
+import android.content.res.Configuration
+import android.content.res.Configuration.ORIENTATION_LANDSCAPE
+import android.content.res.Configuration.ORIENTATION_PORTRAIT
 import android.util.Log
 
 // QuizApplication.kt
@@ -12,5 +15,17 @@ class QuizApplication : Application() {
         super.onCreate()
         Log.d(LOG_TAG, "onCreate() called")
     }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig);
+        Log.d(LOG_TAG, "onConfigurationChanged() called")
+        Log.d(LOG_TAG, "orientation is ${
+            when(newConfig.orientation) {
+                ORIENTATION_LANDSCAPE -> "Landscape"
+                ORIENTATION_PORTRAIT -> "Portrait"
+                else -> "Undefined"
+            }})")
+    }
+
 
 }
